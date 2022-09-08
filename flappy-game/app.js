50f4d032ce8e8774720f999ce43429d4279e316c
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2022-09-08 00:59:23 -0600

    flappy - create a function that generates a random num between 2 values

--- flappy-game/app.js
+++ flappy-game/app.js
@@ -3,7 +3,17 @@ var obstacle = document.querySelector(".obstacle");
 var hole = document.querySelector(".pass");
 
 obstacle.addEventListener("animationiteration", () => {
-    //generate rand num between 150 & 500
-    var random = -1 *  (Math.floor(Math.random() * (500 - 150 + 1)) + 150);
+    //generate a number between 150 & 500
+    var random = -1 * randomNumberGenerator(500, 150);
     hole.style.top = random + 'px';
 });
+
+/**
+ * random number generator
+ * @param {Number} max 
+ * @param {Number} min 
+ * @returns random (integer) number between max & min (both inclusive)
+ */
+function randomNumberGenerator(max, min) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
